6b8ce84612f316d1ee6b283c357b1d0a936f4c9a
Isabella Lopes Pereira <105372986+Bellitah@users.noreply.github.com>
2023-09-24 15:47:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeColor(element) {
+    element.style.background = "linear-gradient(to bottom, #352c63, #473d7a)";
+    element.style.boxShadow = "0 10px 20px rgba(0, 0, 0, 0.4)";
+}
+
+function restoreColor(element) {
+    element.style.background = "linear-gradient(to bottom, #5a52a5, #473d7a)";
+    element.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.3)";
+}
